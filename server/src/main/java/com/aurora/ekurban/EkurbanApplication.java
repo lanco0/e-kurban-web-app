646ea81c5cc3283abf6ce0518bd22e58773061f1
abmehmet <mehmetercan5755@gmail.com>
2022-12-23 16:03:22 +0300

    initial commit

---
+++ server/src/main/java/com/aurora/ekurban/EkurbanApplication.java
@@ -0,0 +1,13 @@
+package com.aurora.ekurban;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EkurbanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EkurbanApplication.class, args);
+	}
+
+}
---
+++ server/src/test/java/com/aurora/ekurban/EkurbanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aurora.ekurban;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EkurbanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
