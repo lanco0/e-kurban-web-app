e043bb9a7126247d550c027bf3de56915f10b727
Harun Can Yildirim <eharuncan@gmail.com>
2023-01-02 14:51:07 +0300

    gerçek apiye dönüşüm sağlandı

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -3,14 +3,11 @@ import {BrowserModule} from '@angular/platform-browser';
 import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import {HttpClientModule} from '@angular/common/http';
 import {AppRoutingModule} from './app-routing.module';
-import {HttpClientInMemoryWebApiModule} from 'angular-in-memory-web-api';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
 import {MatToolbarModule} from "@angular/material/toolbar";
 import {MatIconModule} from "@angular/material/icon";
 import {MatButtonModule} from "@angular/material/button";
-
-import {InMemoryDataService} from './servisler/in-memory-data.service';
 
 import {AppComponent} from './app.component';
 import {AnasayfaComponent} from './komponentler/anasayfa/anasayfa.component';
@@ -30,13 +27,6 @@ import {MatInputModule} from "@angular/material/input";
         ReactiveFormsModule,
         AppRoutingModule,
         HttpClientModule,
-
-        // The HttpClientInMemoryWebApiModule module intercepts HTTP requests
-        // and returns simulated server responses.
-        // Remove it when a real server is ready to receive requests.
-        HttpClientInMemoryWebApiModule.forRoot(
-            InMemoryDataService, {dataEncapsulation: false}
-        ),
         BrowserAnimationsModule,
         FormsModule,
         MatToolbarModule,
