12e078850ea008ce5759f23051ffc284f52365d6
Harun Can Yildirim <eharuncan@gmail.com>
2023-01-02 11:41:52 +0300

    refactoring

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,30 +1,31 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
 
-import { GirisComponent } from "./giris/giris.component";
-import { AnasayfaComponent } from './anasayfa/anasayfa.component';
-import { KurbanlarComponent } from './kurbanlar/kurbanlar.component';
-import { KurbanBilgiComponent } from './kurban-bilgi/kurban-bilgi.component';
+import {GirisComponent} from "./giris/giris.component";
+import {AnasayfaComponent} from './anasayfa/anasayfa.component';
+import {KurbanlarComponent} from './kurbanlar/kurbanlar.component';
 import {KurbanEkleComponent} from "./kurban-ekle/kurban-ekle.component";
+import {KurbanBilgiComponent} from './kurban-bilgi/kurban-bilgi.component';
 import {HissedarlarComponent} from "./hissedarlar/hissedarlar.component";
 import {HissedarEkleComponent} from "./hissedar-ekle/hissedar-ekle.component";
 
 const routes: Routes = [
-  { path: '', redirectTo: '/anasayfa', pathMatch: 'full' },
-  { path: 'giris', component: GirisComponent },
-  { path: 'anasayfa', component: AnasayfaComponent },
-  { path: 'kurbanlar', component: KurbanlarComponent },
-  { path: 'kurban-ekle', component: KurbanEkleComponent },
-  { path: 'kurban-bilgi:id', component: KurbanBilgiComponent },
-  { path: 'hissedarlar', component: HissedarlarComponent },
-  { path: 'hissedar-ekle', component: HissedarEkleComponent },
+    {path: '', redirectTo: '/anasayfa', pathMatch: 'full'},
+    {path: 'giris', component: GirisComponent},
+    {path: 'anasayfa', component: AnasayfaComponent},
+    {path: 'kurbanlar', component: KurbanlarComponent},
+    {path: 'kurban-ekle', component: KurbanEkleComponent},
+    {path: 'kurban-bilgi:id', component: KurbanBilgiComponent},
+    {path: 'hissedarlar', component: HissedarlarComponent},
+    {path: 'hissedar-ekle', component: HissedarEkleComponent},
 ];
 
 @NgModule({
-  imports: [ RouterModule.forRoot(routes) ],
-  exports: [ RouterModule ]
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
 })
-export class AppRoutingModule {}
+export class AppRoutingModule {
+}
 
 
 /*
